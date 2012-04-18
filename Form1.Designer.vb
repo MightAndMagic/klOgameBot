b417ace9a97b8e243f8f233bd0cb7e01b80a0ad7
MightAndMagic <dada.dudu1@web.de>
2012-04-18 17:49:06 +0200

    Nur zu Testzwecken ;)

--- Form1.Designer.vb
+++ Form1.Designer.vb
@@ -1,5 +1,5 @@
 ﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
-Partial Class Form1
+Partial Class klOgameBot
     Inherits System.Windows.Forms.Form
 
     'Das Formular überschreibt den Löschvorgang, um die Komponentenliste zu bereinigen.
@@ -22,17 +22,80 @@ Partial Class Form1
     'Das Bearbeiten mit dem Code-Editor ist nicht möglich.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.textbox = New System.Windows.Forms.TextBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.querybox = New System.Windows.Forms.TextBox()
         Me.SuspendLayout()
         '
-        'Form1
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(9, 62)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(167, 13)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Hier könnte ihre Werbung stehen!"
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(12, 12)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(393, 20)
+        Me.TextBox1.TabIndex = 1
+        '
+        'textbox
+        '
+        Me.textbox.Location = New System.Drawing.Point(12, 12)
+        Me.textbox.Name = "textbox"
+        Me.textbox.Size = New System.Drawing.Size(312, 20)
+        Me.textbox.TabIndex = 1
+        Me.textbox.Text = "http://linkynator99.li.funpic.de"
+        Me.textbox.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(330, 12)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 2
+        Me.Button1.Text = "Los!"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'querybox
+        '
+        Me.querybox.Location = New System.Drawing.Point(12, 38)
+        Me.querybox.Name = "querybox"
+        Me.querybox.Size = New System.Drawing.Size(312, 20)
+        Me.querybox.TabIndex = 3
+        Me.querybox.Text = "Dateien"
+        Me.querybox.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        '
+        'klOgameBot
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(284, 261)
-        Me.Name = "Form1"
-        Me.Text = "Form1"
+        Me.ClientSize = New System.Drawing.Size(417, 210)
+        Me.Controls.Add(Me.querybox)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.textbox)
+        Me.Controls.Add(Me.Label1)
+        Me.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.MaximizeBox = False
+        Me.Name = "klOgameBot"
+        Me.ShowIcon = False
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "klOgameBot"
         Me.ResumeLayout(False)
+        Me.PerformLayout()
 
     End Sub
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
+    Friend WithEvents textbox As System.Windows.Forms.TextBox
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents querybox As System.Windows.Forms.TextBox
 
 End Class
--- Form1.vb
+++ Form1.vb
@@ -1,6 +1,36 @@
-﻿Public Class Form1
-
+﻿Imports System.Net, System.IO
+Public Class klOgameBot
+    Dim url, query As String
     Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
 
+    End Sub
+    Private Function httpGET(ByVal url, ByVal query)
+        Dim request As WebRequest
+        Try
+            request = WebRequest.Create(url)
+        Catch e As Exception
+            MsgBox("Verbindung konnte nicht hergestellt werden. Bitte gib eine gültige URL ein!", , "Fehler!")
+            Return ""
+        End Try
+        request.Method = "GET"
+        Dim response As HttpWebResponse = request.GetResponse
+        Dim stream As Stream = response.GetResponseStream
+        Dim reader As New StreamReader(stream)
+        Dim result = reader.ReadToEnd
+        reader.Close()
+        response.Close()
+        verarbeite(result, query)
+        Return result
+    End Function
+
+    Private Sub Button1_Click(sender As System.Object, e As System.EventArgs) Handles Button1.Click
+        url = textbox.Text
+        query = querybox.Text
+        Label1.Text = httpGET(url, query)
+    End Sub
+    Private Sub verarbeite(ByVal result, ByVal query)
+        If InStr(result, query) Then
+            MsgBox("'" + query + "' gefunden!", MsgBoxStyle.Information, "Erfolg!")
+        End If
     End Sub
 End Class
